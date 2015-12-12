5487616cd580011b6dfd604ab089c8521d3e3c51
Joe Huss <detain@interserver.net>
2015-12-12 12:15:11 -0500

    Initial work on the new composer iisntaller plugin  to handle installing things to there proper locations

---
+++ include/Composer/Installer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace detain\Composer;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class TemplateInstaller extends LibraryInstaller
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        $prefix = substr($package->getPrettyName(), 0, 23);
+        if ('myadmin/template-' !== $prefix) {
+            throw new \InvalidArgumentException(
+                'Unable to install template, myadmin templates '
+                .'should always start their package name with '
+                .'"myadmin/template-"'
+            );
+        }
+
+        return 'data/templates/'.substr($package->getPrettyName(), 23);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return 'myadmin-template' === $packageType;
+    }
+}
