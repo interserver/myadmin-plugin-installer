bc151ac34a166ed52350cb58358c74d87bd148c5
Joe Huss <detain@interserver.net>
2017-02-16 19:48:17 -0500

    more work on the myadmin plugin handler

--- src/InstallerPlugin.php
+++ src/InstallerPlugin.php
@@ -4,7 +4,7 @@
  * Implements https://github.com/composer/composer/blob/master/src/Composer/Plugin/PluginInterface.php
  */
 
-namespace detain\myAdmin\Plugins;
+namespace MyAdmin\Plugins;
 
 use Composer\Composer;
 use Composer\IO\IOInterface;
@@ -16,13 +16,19 @@ use Composer\Plugin\PluginInterface;
 class InstallerPlugin implements PluginInterface
 {
 	/**
+	 * Version number of the internal composer-plugin-api package
+	 *
+	 * @var string
+	 */
+	const PLUGIN_API_VERSION = '1.1.0';
+
+	/**
 	 * Apply plugin modifications to Composer
 	 *
-	 * @param Composer\Composer    $composer
+	 * @param Composer\Composer	$composer
 	 * @param Composer\IO\IOInterface $io
 	 */
-	public function activate(Composer\Composer $composer, Composer\IO\IOInterface $io)
-	{
+	public function activate(Composer\Composer $composer, Composer\IO\IOInterface $io) {
 		$installer = new Installer($io, $composer);
 		$composer->getInstallationManager()->addInstaller($installer);
 	}
