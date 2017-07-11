9ab85c418fa83c2917d8e11e3fb731d559b85ffd
Joe Huss <detain@interserver.net>
2017-07-11 06:11:26 -0400

    fixes

--- src/Command/Parse.php
+++ src/Command/Parse.php
@@ -14,6 +14,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Composer\Command\BaseCommand;
 use phpDocumentor\Reflection\Php\Project;
+use phpDocumentor\Reflection\Php\ProjectFactory;
 
 /**
  * Code Parser Comand
