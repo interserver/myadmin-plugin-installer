1e3266a57da0546e003ca6f6658f33bfaffd6400
Joe Huss <detain@interserver.net>
2021-03-19 06:32:28 -0400

    update

--- src/Plugin.php
+++ src/Plugin.php
@@ -45,6 +45,14 @@ class Plugin implements PluginInterface, EventSubscriberInterface, Capable
 		$installer = new Installer($this->io, $this->composer);
 		$this->composer->getInstallationManager()->addInstaller($installer);
 	}
+	
+	public function deactivate(Composer $composer, IOInterface $io)
+	{
+	}
+
+	public function uninstall(Composer $composer, IOInterface $io)
+	{
+	}	
 
 	/**
 	 * @return array
