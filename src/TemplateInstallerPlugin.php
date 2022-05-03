1200c3f5a9ac0e18bf179e10db0dfb122512fb9d
Joe Huss <detain@interserver.net>
2022-05-03 12:30:55 -0400

    update

--- src/TemplateInstallerPlugin.php
+++ src/TemplateInstallerPlugin.php
@@ -10,16 +10,22 @@
 namespace MyAdmin\Plugins;
 
 use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Plugin\PluginEvents;
+use Composer\Plugin\PreFileDownloadEvent;
 
 /**
  * Class TemplateInstallerPlugin
  *
  * @package MyAdmin\Plugins
  */
-class TemplateInstallerPlugin implements PluginInterface
+class TemplateInstallerPlugin implements PluginInterface, EventSubscriberInterface
 {
+	protected $composer;
+	protected $io;
+
 	/**
 	 * @param \Composer\Composer       $composer
 	 * @param \Composer\IO\IOInterface $io
@@ -29,4 +35,37 @@ class TemplateInstallerPlugin implements PluginInterface
 		$installer = new TemplateInstaller($io, $composer);
 		$composer->getInstallationManager()->addInstaller($installer);
 	}
+
+	public function activate(Composer $composer, IOInterface $io)
+	{
+		$this->composer = $composer;
+		$this->io = $io;
+	}
+
+	public function deactivate(Composer $composer, IOInterface $io)
+	{
+	}
+
+	public function uninstall(Composer $composer, IOInterface $io)
+	{
+	}
+
+	public static function getSubscribedEvents()
+	{
+		return array(
+			PluginEvents::PRE_FILE_DOWNLOAD => array(
+				array('onPreFileDownload', 0)
+			),
+		);
+	}
+
+	public function onPreFileDownload(PreFileDownloadEvent $event)
+	{
+		$protocol = parse_url($event->getProcessedUrl(), PHP_URL_SCHEME);
+	}
+}
+
+
+
+
 }
